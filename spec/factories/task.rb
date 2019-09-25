4d32e5a3cfaed91616e4e87be1801a2716f2820a
janvier mutuyimana <janvierjavim2@gmail.com>
2019-09-25 16:59:00 +0000

    testing

---
+++ spec/factories/task.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+
+    factory :task do
+      name  'test' 
+      details  'hello guys' 
+    end
+  
+    
+    factory :second_task, class: Task do
+      name 'Default title 2 created by Factory' 
+      details  'Default content 2 created by Factory' 
+    end
+  end
