70781c127c3ab3bf9f8693b6d815248032c74661
Janvierjavi <janvierjavim2@gmail.com>
2019-10-28 07:21:05 +0000

    i remove user and session

--- app/models/user.rb
+++
@@ -1,8 +0,0 @@
-class User < ApplicationRecord
-    validates :name,  presence: true, length: { maximum: 150 }
-  validates :email, presence: true, uniqueness: true, length: { maximum: 255 },
-                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-  has_many :tasks
-  has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
-end
