f592442cd848a5c43c56c7c0d0bc745b90ce2ac3
Janvierjavi <janvierjavim2@gmail.com>
2019-11-14 07:04:43 +0000

    i add some conditon for the task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,7 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
+    
       @search = Task.ransack(params[:q])
        if params[:q]
          @tasks = @search.result.page(params[:page])
